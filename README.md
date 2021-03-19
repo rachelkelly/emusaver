0977f541d4c9499b6bd10ae86ec1a64ff65f4460
Rachel Kelly <subs@rkode.com>
2021-03-18 23:11:14 -0700

    first part of the tarby & env

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # emusaver
+
+A program to save ff5 files (for now) and give them a uniqueish name, and crank em up to s3, & pull em back down too.
--- gamesender.sh
+++ gamesender.sh
@@ -2,4 +2,13 @@
 
 # a tool to send emulation save kits (a term I am making up) up to AWS S3
 
+# get location of saves, bundle up entirety of save file
+
+computerdate=$(date +%s)
+humandate=$(date %F)
+savebundle="ff5$humandate$epoch.tar.gz"
+
+#tar -czvf $savebundle -X exclude.txt "/mnt/c/snes stuff/EMUPROJ/"
+tar -czvf $savebundle "/mnt/c/snes stuff/EMUPROJ/"
+tar tvf $savebundle
 #aws s3 cp 
