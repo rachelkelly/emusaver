e3e49f1d605dfe65df0e38b050d1cf1750a506e4
Rachel Kelly <subs@rkode.com>
2021-03-22 23:20:29 -0700

    had some problematic slashes and other lil issues

--- gamesender.sh
+++ gamesender.sh
@@ -2,7 +2,7 @@
 
 # a tool to send emulation save kits (a term I am making up) up to AWS S3
 # if using this just for FF5, the only var needing definition is the complete linux-style 
-# path to the windows location.  /mnt/c/snes stuff/EMUPROJ/ is just mine.
+# path to the windows location.  /mnt/c/snesstuff/EMUPROJ/ is just mine.
 
 # usage: ./gamesender.sh send
 #        ./gamesender.sh get
@@ -11,30 +11,23 @@ computerdate=$(date +%s)
 humandate=$(date +%F)
 game="ff5"
 sendbundle="$game-$humandate-epoch$computerdate.tar.gz"
-location="/mnt/c/snesstuff/EMUPROJ/"
-bucket="s3://emusaver/"
-
-# janky argsv, just for you 😘
-if [[ $1 == "send" ]]; then
-    send
-elif [[ $1 == "get" ]]; then
-    get
-fi
+location="/mnt/c/snesstuff/EMUPROJ"
+bucket="s3://emusaves"
 
 # tarballs & sends bundle
 send() {
-    tar -czvf $sendbundle $location
+    tar -czvf $sendbundle $location/current
     tar tvf $sendbundle
-    aws s3 cp $sendbundle $bucket
+    aws s3 cp $sendbundle $bucket/
 }
 
 # first, we secure the bundle that might be local
 # looks for newest object in bucket based on its timestamp (not the date on the label)
 get() {
     assurance
-    newest=$(aws s3 ls s3://emusaves | sort | tail -n 1 | awk '{print $4}')
-    aws s3 cp $bucket/$newest $location
-    cd $location && tar -xzvf $newest
+    newest=$(aws s3 ls $bucket | sort | tail -n 1 | awk '{print $4}')
+    aws s3 cp $bucket/$newest $location/current/
+    cd $location/current && tar -xzvf $newest
 }
 
 assurance() {
@@ -42,5 +35,12 @@ assurance() {
     mv $location/current/* $location/backup/
     echo "Current batch backed up before sending"
 }
+
+# janky argsv, just for you 😘
+if [[ $1 == "send" ]]; then
+    send
+elif [[ $1 == "get" ]]; then
+    get
+fi
 
 
